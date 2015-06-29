d4ca32218a558c7415ec2de123b1011fcf48b17f
Nick Tönissen <n.tonissen@student.science.ru.nl>
2015-06-29 21:27:21 +0200

    Beter key check

--- app.py
+++ app.py
@@ -28,6 +28,16 @@ class Response(Base):
 API_KEY = os.environ['HENRY_API_KEY']
 
 dict = {}
+
+def parse_message(msg, data):
+    if 'username' in data['message']['from']:
+        username = "@" + data['message']['from']['username']
+    elif 'first_name' in data['message']['from']:
+        username = data['message']['from']['first_name']
+    else:
+        username = 'Unknown'
+    return msg.replace("{user}", username)
+
 def parse_response(json):
     if 'text' in json['message']:
         match = re.search(r'(\w+):([^:]+):(.+)', json['message']['text'])
@@ -36,44 +46,49 @@ def parse_response(json):
                 pattern = match.group(2)
                 answer = match.group(3)
                 if len(pattern) > 2:
-                    dict[pattern] = answer
-                    session.add(Response(pattern,answer))
+                    dict[pattern.upper()] = answer
+                    session.add(Response(pattern.upper(),answer))
                     session.commit()
             elif match.group(1) == "henry":
                 if match.group(2) == "delete":
-                    if match.group(3) in dict.keys():
-                        dict.pop(match.group(3))
-                        responses = session.query(Response).filter_by(trigger=match.group(3)).all()
+                    pattern = match.group(3)[:-1]
+                    pattern_capi = pattern.upper()
+                    if pattern_capi in dict.keys():
+                        dict.pop(pattern_capi)
+                        responses = session.query(Response).filter_by(trigger=pattern).all()
                         for res in responses:
                             session.delete(res)
                         session.commit()
                 elif match.group(2) == "dump":
-                    requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(API_KEY, json['message']['chat']['id'], ",".join(map(str,dict.keys())).encode('utf8')))
+                    requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(API_KEY, json['message']['chat']['id'], "\n".join(map(str,dict.keys())).encode('utf8')))
         else:
             answer = ""
             for key in dict.keys():
-                if key.upper() in json['message']['text'].upper():
+				rex = re.escape(key) + r'([\s\?!.,:;])?$'
+				match = re.search(rex, json['message']['text'].upper(), re.IGNORECASE)
+                if match
                     answer += dict[key]
                     answer += "\n"
 
             if answer != "":
-                requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(API_KEY, json['message']['chat']['id'], answer.encode('utf8')))
+                requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(API_KEY, json['message']['chat']['id'], parse_message(answer,json).encode('utf8')))
 
 
 def loop(id = 0):
     data = requests.get("https://api.telegram.org/bot{}/getUpdates?offset={}&timeout=30".format(API_KEY, id))
     data.encoding = 'utf8'
-    json = data.json(encoding='utf8')
-    result = json['result']
-    if len(result) > 0:
-        id = result[0]['update_id']
-        parse_response(result[0])
+    if data is not None:
+        json = data.json(encoding='utf8')
+        result = json['result']
+        if len(result) > 0:
+            id = result[0]['update_id']
+            parse_response(result[0])
     return id
 
 if __name__ == '__main__':
     q = session.query(Response).all()
     for response in q:
-        dict[response.trigger] = response.response
+        dict[response.trigger.upper()] = response.response
     id = 0
     while True:
         id = loop(id) + 1
